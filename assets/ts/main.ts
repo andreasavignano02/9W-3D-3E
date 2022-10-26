51821c5a71f3aec87e5bfd1a1d091fd8a6da32a8
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-10-26 16:03:18 +0200

    fix start

---
+++ assets/ts/main.ts
@@ -0,0 +1,10 @@
+class Lavoratori {
+    codredd? : number;
+    redditoannuo? : number;
+    tasseinps?: number;
+    tasseirpf?: number;
+}
+
+abstract class ingegnere extends Lavoratori{
+    super()
+}
