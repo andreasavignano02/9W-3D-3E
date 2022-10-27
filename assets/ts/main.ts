435c2acbd49c51a1441f8770d8ba70fd04c1c8a3
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-10-27 09:38:00 +0200

    fix

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -3,8 +3,23 @@ class Lavoratori {
     redditoannuo? : number;
     tasseinps?: number;
     tasseirpf?: number;
+    constructor(codredd? : number, redditoannuo? : number, tasseinps?: number,tasseirpf?: number){
+        this.codredd = codredd;
+        this.redditoannuo = redditoannuo;
+        this.tasseinps = tasseinps;
+        this.tasseirpf = tasseirpf;
+    };
+    gettasseirpf(){
+        console.log(this.tasseirpf);
+    };
+    gettasseinps(){
+        console.log(this.tasseinps);
+    };
+    gettassetotali(){
+        let totaletasse = this.tasseinps + this.tasseirpf;
+        return totaletasse
+    }
 }
-
-abstract class ingegnere extends Lavoratori{
-    super()
+abstract class ingegnere extends Lavoratori {
+    
 }
